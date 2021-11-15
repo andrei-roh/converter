cb64eb9f483abed86296b09d4e1d76fd1385ac49
Andrei Roh <65450338+andrei-roh@users.noreply.github.com>
2021-11-15 19:44:46 +0300

    Added test some functions. Updated app styles

--- src/components/Content/components/TableField/components/ScrollOnTop/style.js
+++ src/components/Content/components/TableField/components/ScrollOnTop/style.js
@@ -11,7 +11,7 @@ export const ButtonBlock = styled.div`
   }
 `;
 
-export const ButtonField = styled.div`
+export const ButtonField = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -21,9 +21,24 @@ export const ButtonField = styled.div`
   height: 32px;
   margin: 16px 0;
   z-index: 1;
+  cursor: pointer;
+  color: ${({ theme }) => theme.text};
+  background: ${({ theme }) => theme.body};
 
   &:hover {
     box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
       0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
+  }
+
+  &:focus {
+    border: solid #3f51b5 1px;
+    outline: solid #3f51b5 1px;
+    color: #3f51b5;
+  }
+
+  &:focus-visible {
+    border: solid #3f51b5 1px;
+    outline: solid #3f51b5 1px;
+    color: #3f51b5;
   }
 `;
